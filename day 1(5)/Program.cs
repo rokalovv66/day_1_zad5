217f37e05f1423f407337827edd52616e07b55ad
Нисик <@DESKTOP-ALORV4C>
2023-04-17 14:15:08 +0300

    Добавьте файлы проекта.

---
+++ day 1(5)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int max = (num1 > num2) ? num1 : num2;
+
+        Console.WriteLine("Наибольшее число: " + max);
+    }
+}
